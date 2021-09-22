f3150f49ce3fab33ebea73737239318de0b9879a
USER <USER@shrutikajaulkar>
2021-09-22 10:25:32 +0530

    student dao file

--- src/com/dao/StdDao.java
+++ src/com/dao/StdDao.java
@@ -2,6 +2,7 @@ package com.dao;
 import java.util.List;
 
 public interface StdDao<Student> {
+	int getstdid = 0;
 	public boolean add(Student s);
 	public boolean delete(int sid);
 	public List<Student>getAllStd();
@@ -10,6 +11,17 @@ public interface StdDao<Student> {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+	public static void settStdid(int int1) {
+		// TODO Auto-generated method stub
+		
+	}
+	public static void setStdnm(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	public int getstdid();
+	public String getstdnm();
+	List<StdDao> getAllStdDao();
 }
 	
 
